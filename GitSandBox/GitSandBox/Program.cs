a70341ad09da60a19b6c167c7d7e5d3cb1c284af
Aditya <aditya.ajjarapu@spiralogics.com>
2020-09-24 11:55:06 +0530

    B01C001

--- GitSandBox/GitSandBox/Program.cs
+++ GitSandBox/GitSandBox/Program.cs
@@ -6,7 +6,7 @@ namespace GitSandBox
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! from B1");
         }
     }
 }
