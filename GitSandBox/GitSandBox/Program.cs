be8936848b17c2ff9a4a182b6da46f71812005dc
Aditya <aditya.ajjarapu@spiralogics.com>
2020-09-24 11:56:45 +0530

    M01C001

--- GitSandBox/GitSandBox/Program.cs
+++ GitSandBox/GitSandBox/Program.cs
@@ -7,6 +7,7 @@ namespace GitSandBox
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Commited In Master");
         }
     }
 }
